f968c8a583a7e520892117d4bd5bf25047f89a8a
BohdanSereda <zxysereda.bodik@gmail.com>
2022-03-11 17:44:51 +0200

    Add new dependencies

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import express  from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
 import {router as authRoutes} from './routes/auth.js' 
 import {router as analyticsRoutes} from './routes/analytics.js' 
 import {router as categoryRoutes} from './routes/category.js' 
@@ -7,9 +9,15 @@ import {router as positionRoutes} from './routes/position.js'
 
 const app = express()
 
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+
 app.use('/api/auth', authRoutes)
 app.use('/api/analytics', analyticsRoutes)
 app.use('/api/category', categoryRoutes)
 app.use('/api/order', orderRoutes)
 app.use('/api/position', positionRoutes)
+
 export {app}
