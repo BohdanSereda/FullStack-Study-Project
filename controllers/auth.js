0e8694f6e9a51d4380932bae117ecdfab3ebae65
BohdanSereda <zxysereda.bodik@gmail.com>
2022-03-25 22:40:12 +0200

    Add register function implementation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,30 @@
+import bcrypt from 'bcrypt'
+import User from '../models/User.js'
+
 const login = (req, res) =>{
-    res.status(200).json({
-        login: "from controller"
-    })
+    const userData = {
+      login: req.body
+    }
+    res.status(200).json(userData)
 }
-const register = (req, res)=>{
-    res.status(200).json({
-        register: "from controller"
+const  register = async (req, res)=>{
+    const existingUser = await  User.findOne({email: req.body.email})
+    if(existingUser){
+        res.status(409).json({
+            message: 'Error: email has already been taken'
+        })
+    }
+    const salt = bcrypt.genSaltSync(10)
+    const password = req.body.password
+    const user = new User({
+        email: req.body.email,
+        password: bcrypt.hashSync(password, salt)
     })
+    try{
+        await user.save()
+        res.status(201).json(user)
+    }catch(error){
+
+    }
 }
 export {login, register}
